f740e014eac8bded5806be9de058fb6e5e4e3b23
Steven Pickering <52013044+Steven909@users.noreply.github.com>
2021-10-22 23:57:42 +0100

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+
+        String words = "To be or not to be, that is the question";
+
+        String[] splitWords = words.split(" ");
+        int numberOfStrings = splitWords.length;
+        System.out.println("Number of strings is > " + numberOfStrings);
+    }
+
+}
+
+
